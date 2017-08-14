d073faae47cf6af2fb6f5f4b3ab40cdc51630fc9
christian <gudim87@hotmail.com>
2017-08-14 12:03:59 +0200

    Add secure password and attribute to user

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,6 @@ end
   def create
     @article = Article.new(article_params)
     @article.user = User.first #This is hardcoded so i can save the article, will change this later
-
     if @article.save
       flash[:success] = "Article was successfully created"
       redirect_to article_path(@article)
