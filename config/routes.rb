6e8bb0e1b8dd1400e94b98c7723fb27988705a87
christian <gudim87@hotmail.com>
2017-08-28 17:33:32 +0200

    add users controllers

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
 
   resources :articles
+
+  get 'signup', to: 'users#new'
+  post 'users', to: 'users#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
