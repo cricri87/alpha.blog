aa72bbf207b344604b23b6466cba16e7b5dd3cf5
christian <gudim87@hotmail.com>
2017-09-13 17:40:37 +0200

    Add edit update functionality and partial

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,12 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
 
   resources :articles
+  resources :users
 
   get 'signup', to: 'users#new'
   post 'users', to: 'users#create'
+  
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
