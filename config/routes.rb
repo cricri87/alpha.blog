a3419350008af74512d6148f9ce7414680b73b76
christian <gudim87@hotmail.com>
2017-09-16 17:24:18 +0200

    complite authenticateion system

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,15 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
 
   resources :articles
-  resources :users
+  resources :users, exept: [:new]
 
   get 'signup', to: 'users#new'
   post 'users', to: 'users#create'
-  
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
